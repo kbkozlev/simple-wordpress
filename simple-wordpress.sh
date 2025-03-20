f50e8ea29d9e4850f75625dd6b9ad78e66e4b9d0
Kaloian Kozlev <kbkozlev@gmail.com>
2025-03-20 13:00:03 +0200

    Update simple-wordpress.sh

--- simple-wordpress.sh
+++ simple-wordpress.sh
@@ -1,83 +1,72 @@
 #!/bin/sh
 
-# Simple Menu for Installing or Reinstalling WordPress
-echo "Select an option:"
-echo "1. Install WordPress"
-echo "2. Reinstall WordPress (Overwrite everything)"
-echo "3. Exit"
+install_dir="/var/www/html"
+#Creating Random WP Database Credenitals
+db_name="wp$(date +%s)"
+db_user=$db_name
+db_password=$(date | md5sum | cut -c '1-12')
+sleep 1
+mysqlrootpass=$(date | md5sum | cut -c '1-12')
+sleep 1
 
-read -p "Enter your choice: " choice
+# Install Packages for https and mysql
+apt -y update
+apt -y upgrade
+apt -y install apache2
+apt -y install mysql-server
 
-# Function to install WordPress
-install_wordpress() {
-    echo "Starting WordPress installation..."
+# Start http
+rm /var/www/html/index.html
+systemctl enable apache2
+systemctl start apache2
 
-    install_dir="/var/www/html"
-    # Creating Random WP Database Credentials
-    db_name="wp$(date +%s)"
-    db_user="$db_name"
-    db_password=$(date | md5sum | cut -c '1-12')
-    sleep 1
-    mysqlrootpass=$(date | md5sum | cut -c '1-12')
-    sleep 1
+# Start mysql and set root password
+systemctl enable mysql
+systemctl start mysql
 
-    # Install Packages for HTTPS and MySQL
-    apt -y update
-    apt -y upgrade
-    apt -y install apache2 mysql-server php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml php-cli phpmyadmin wget lynx
+/usr/bin/mysql -e "USE mysql;"
+/usr/bin/mysql -e "UPDATE user SET Password=PASSWORD('$mysqlrootpass') WHERE user='root';"
+/usr/bin/mysql -e "FLUSH PRIVILEGES;"
+touch /root/.my.cnf
+chmod 640 /root/.my.cnf
+echo "[client]" >> /root/.my.cnf
+echo "user=root" >> /root/.my.cnf
+echo "password=$mysqlrootpass" >> /root/.my.cnf
 
-    # Start Apache
-    rm -f /var/www/html/index.html
-    systemctl enable apache2
-    systemctl start apache2
+# Install PHP
+apt -y install php php-bz2 php-mysqli php-curl php-gd php-intl php-common php-mbstring php-xml php-cli phpmyadmin
 
-    # Start MySQL and Set Root Password
-    systemctl enable mysql
-    systemctl start mysql
+sed -i '0,/AllowOverride None/! {0,/AllowOverride None/ s/AllowOverride None/AllowOverride All/}' /etc/apache2/apache2.conf # Allow htaccess usage
 
-    /usr/bin/mysql -e "USE mysql;"
-    /usr/bin/mysql -e "UPDATE user SET Password=PASSWORD('$mysqlrootpass') WHERE user='root';"
-    /usr/bin/mysql -e "FLUSH PRIVILEGES;"
+systemctl restart apache2
 
-    touch /root/.my.cnf
-    chmod 600 /root/.my.cnf  # Secure the .my.cnf file
-    echo "[client]" > /root/.my.cnf
-    echo "user=root" >> /root/.my.cnf
-    echo "password=$mysqlrootpass" >> /root/.my.cnf
+# Download and extract latest WordPress Package
+if test -f /tmp/latest.tar.gz; then
+    echo "WP is already downloaded."
+else
+    echo "Downloading WordPress"
+    cd /tmp/ && wget "http://wordpress.org/latest.tar.gz";
+fi
 
-    # Allow .htaccess in Apache
-    sed -i '0,/AllowOverride None/! {0,/AllowOverride None/ s/AllowOverride None/AllowOverride All/}' /etc/apache2/apache2.conf
+/bin/tar -C $install_dir -zxf /tmp/latest.tar.gz --strip-components=1
+chown www-data: $install_dir -R
 
-    systemctl restart apache2
+# Create WP-config and set DB credentials
+/bin/mv $install_dir/wp-config-sample.php $install_dir/wp-config.php
+/bin/sed -i "s/database_name_here/$db_name/g" $install_dir/wp-config.php
+/bin/sed -i "s/username_here/$db_user/g" $install_dir/wp-config.php
+/bin/sed -i "s/password_here/$db_password/g" $install_dir/wp-config.php
 
-    # Download and Extract Latest WordPress Package
-    if [ -f /tmp/latest.tar.gz ]; then
-        echo "WP is already downloaded."
-    else
-        echo "Downloading WordPress"
-        cd /tmp/ && wget "http://wordpress.org/latest.tar.gz"
-    fi
-
-    /bin/tar -C "$install_dir" -zxf /tmp/latest.tar.gz --strip-components=1
-    chown www-data:www-data "$install_dir" -R
-    rm -f /tmp/latest.tar.gz  # Clean up the downloaded tarball
-
-    # Create WP-config and Set DB Credentials
-    /bin/mv "$install_dir/wp-config-sample.php" "$install_dir/wp-config.php"
-    /bin/sed -i "s/database_name_here/$db_name/g" "$install_dir/wp-config.php"
-    /bin/sed -i "s/username_here/$db_user/g" "$install_dir/wp-config.php"
-    /bin/sed -i "s/password_here/$db_password/g" "$install_dir/wp-config.php"
-
-    cat << EOF >> "$install_dir/wp-config.php"
+cat << EOF >> $install_dir/wp-config.php
 define('FS_METHOD', 'direct');
 EOF
 
-    cat << EOF >> "$install_dir/.htaccess"
+cat << EOF >> $install_dir/.htaccess
 # BEGIN WordPress
 <IfModule mod_rewrite.c>
 RewriteEngine On
 RewriteBase /
-RewriteRule ^index.php$ - [L]
+RewriteRule ^index.php$ – [L]
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME} !-d
 RewriteRule . /index.php [L]
@@ -85,59 +74,23 @@ RewriteRule . /index.php [L]
 # END WordPress
 EOF
 
-    chown www-data:www-data "$install_dir" -R
+chown www-data: $install_dir -R
 
-    # Set WP Salts
-    grep -A50 'table_prefix' "$install_dir/wp-config.php" > /tmp/wp-tmp-config
-    /bin/sed -i '/\/**#@/,/$p/d' "$install_dir/wp-config.php"
-    /usr/bin/lynx --dump -width 200 https://api.wordpress.org/secret-key/1.1/salt/ >> "$install_dir/wp-config.php"
-    /bin/cat /tmp/wp-tmp-config >> "$install_dir/wp-config.php" && rm -f /tmp/wp-tmp-config
+# Set WP Salts
+grep -A50 'table_prefix' $install_dir/wp-config.php > /tmp/wp-tmp-config
+/bin/sed -i '/**#@/,/$p/d' $install_dir/wp-config.php
+/usr/bin/lynx --dump -width 200 https://api.wordpress.org/secret-key/1.1/salt/ >> $install_dir/wp-config.php
+/bin/cat /tmp/wp-tmp-config >> $install_dir/wp-config.php && rm /tmp/wp-tmp-config -f
 
-    # Create Database and User
-    /usr/bin/mysql -u root -e "CREATE DATABASE $db_name"
-    /usr/bin/mysql -u root -e "CREATE USER '$db_user'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';"
-    /usr/bin/mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
+# Create Database and User
+/usr/bin/mysql -u root -e "CREATE DATABASE $db_name"
+/usr/bin/mysql -u root -e "CREATE USER '$db_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';"
+/usr/bin/mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
 
-    # Display generated passwords to log file.
-    echo "Database Name: $db_name"
-    echo "Database User: $db_user"
-    echo "Database Password: $db_password"
-    echo "Mysql root password: $mysqlrootpass"
-    echo "phpMyAdmin User: $db_user"
-    echo "phpMyAdmin Password: $db_password"
-}
-
-# Function to reinstall everything (delete and re-install WordPress)
-reinstall_wordpress() {
-    echo "Reinstalling WordPress... This will overwrite everything!"
-
-    # Stop Apache and MySQL services before cleanup
-    systemctl stop apache2
-    systemctl stop mysql
-
-    # Remove existing WordPress files and database
-    rm -rf /var/www/html/*
-    /usr/bin/mysql -u root -e "DROP DATABASE IF EXISTS $db_name;"
-    /usr/bin/mysql -u root -e "DROP USER IF EXISTS '$db_user'@'localhost';"
-
-    # Re-run the install function
-    install_wordpress
-}
-
-# Case to handle the menu selection
-case "$choice" in
-    1)
-        install_wordpress
-        ;;
-    2)
-        reinstall_wordpress
-        ;;
-    3)
-        echo "Exiting script."
-        exit 0
-        ;;
-    *)
-        echo "Invalid choice, exiting."
-        exit 1
-        ;;
-esac
+###### Display generated passwords to log file.
+echo "Database Name: $db_name"
+echo "Database User: $db_user"
+echo "Database Password: $db_password"
+echo "Mysql root password: $mysqlrootpass"
+echo "phpMyAdmin User: $db_user"
+echo "phpMyAdmin Password: $db_password"
